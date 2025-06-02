e51d79c049c8d05c68a3a0323c5b53e71e784fc7
NazmulTakbir <takbir.nazmul@gmail.com>
2025-06-02 14:50:06 -0700

    Added Script for Benchmarking

---
+++ src/script/benchmark_tasks.py
@@ -0,0 +1,37 @@
+benchmark_tasks = [
+    {
+        "task_name":        "code_generation",
+        "task_description": "MULTITHREADED BLOCKED MATRIX MULTIPLICATION IN C++",
+        "n_rounds": 3,
+        "n_samples": 5,
+        "n_voters": 5
+    },
+    {
+        "task_name":        "code_generation",
+        "task_description": "Pytorch implementation of original Transformer paper's encoder/decoder model",
+        "n_rounds": 3,
+        "n_samples": 3,
+        "n_voters": 3
+    },
+    {
+        "task_name":        "code_generation",
+        "task_description": "C++ implementation of the A* algorithm",
+        "n_rounds": 2,
+        "n_samples": 3,
+        "n_voters": 3
+    },
+    {
+        "task_name":        "code_generation",
+        "task_description": "C++ implementation of lockless concurrent queue, similar to the Michael & Scott lock-free queue",
+        "n_rounds": 3,
+        "n_samples": 4,
+        "n_voters": 3
+    },
+    {
+        "task_name":        "code_generation",
+        "task_description": "Pytorch implementation of Vision Transformer (ViT) from scratch",
+        "n_rounds": 3,
+        "n_samples": 5,
+        "n_voters": 3
+    },
+]
